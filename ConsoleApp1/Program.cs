9f30c5bbc6f1d7c43f162a1861d62b144fd83a82
ahmad mdy <ahmad mdy@DESKTOP-TODG5IK>
2024-10-21 00:11:46 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+for(int a = 0; a < 10; a++)
+{
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    if (num == num2)
+    {
+        Console.WriteLine("Bordy");
+        break;
+    }
+    else if (num <= num2)
+    {
+        Console.WriteLine("Bozorgeh");
+    }
+    else if (num >= num2)
+    {
+        Console.WriteLine("kochik");
+    }
+    
+}
+Console.WriteLine("OK");
