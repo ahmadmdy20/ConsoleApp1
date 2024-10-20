6fd388594818a9da73db694f19164fd4ccf97727
ahmadmdy20 <milad.mm2021@gmail.com>
2024-10-21 00:16:34 +0330

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ for(int a = 0; a < 10; a++)
     int num2 = Convert.ToInt32(Console.ReadLine());
     if (num == num2)
     {
-        Console.WriteLine("Bordy");
+        Console.WriteLine("Borsy");
         break;
     }
     else if (num <= num2)
